28bc0ee8eae547377e7ac1c23fcb02f2ec23e451
prakhar070 <bansalprakhar.2266@gmail.com>
2019-07-01 15:34:48 +0530

    finding centres and mail feature included

--- conf/settings.py
+++ conf/settings.py
@@ -149,4 +149,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'bansalprakhar.2266@gmail.com'
-EMAIL_HOST_PASSWORD = 'GMAILsnppp'
+EMAIL_HOST_PASSWORD = '*********'
